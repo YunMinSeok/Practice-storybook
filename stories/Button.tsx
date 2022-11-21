d0040369db9d9fe776b47571a6b535d7e1280df8
윤민석 <ykmvm147@naver.com>
2022-11-21 22:33:43 +0900

    feat: styled-compontents => emotion으로 변경 및 button 컴포넌트 새로 만드는 중

--- stories/Button.tsx
+++ stories/Button.tsx
@@ -1,48 +1,18 @@
-import React from 'react';
-import './button.css';
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import React from "react";
 
-interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
-}
+type ButtonProps = {
+  children: React.ReactNode;
+  onClick?: (e?: React.MouseEvent<HTMLButtonElement>) => void;
+};
 
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+const Button = ({ children, onClick }: ButtonProps) => {
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
-      {label}
+    <button css={style} onClick={onClick}>
+      {children}
     </button>
   );
 };
+
+export default Button;
