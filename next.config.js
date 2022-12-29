121ece774eb0ef200fdaada1687ae6577e63105c
윤민석 <ykmvm147@naver.com>
2022-12-29 23:09:25 +0900

    feat: next.config.js loader 추가 완료

--- next.config.js
+++ next.config.js
@@ -2,8 +2,15 @@ module.exports = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
+      issuer: { and: [/\.(js|ts|md)x?$/] },
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: { plugins: [{ removeViewBox: false }] },
+          },
+        },
+      ],
     });
     return config;
   },
