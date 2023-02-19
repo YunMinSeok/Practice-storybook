d0c63381976502a45d454aa6e77844ba7fe5803b
윤민석 <ykmvm147@naver.com>
2023-02-19 15:22:32 +0900

    feat: svgr 라이브러리 삭제 및 관련 적용 시킨 곳 rollback 완료

--- next.config.js
+++ next.config.js
@@ -1,17 +1,5 @@
-module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: { and: [/\.(js|ts|md)x?$/] },
-      use: [
-        {
-          loader: "@svgr/webpack",
-          options: {
-            svgoConfig: { plugins: [{ removeViewBox: false }] },
-          },
-        },
-      ],
-    });
-    return config;
-  },
+const nextConfig = {
+  /* config options here */
 };
+
+export default nextConfig;
