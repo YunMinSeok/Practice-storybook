2a03f806e067146b9f7ca744110705e9798fc651
윤민석 <ykmvm147@naver.com>
2023-02-21 17:42:36 +0900

    feat: svg index 파일 default 삭제

--- stories/icon/svg/index.ts
+++ stories/icon/svg/index.ts
@@ -1,3 +1,3 @@
-export default { ReactComponent as exit } from "./exit.svg";
+export { ReactComponent as exit } from "./exit.svg";
 export { ReactComponent as heart } from "./heart.svg";
 export { ReactComponent as pencil } from "./pencil.svg";
